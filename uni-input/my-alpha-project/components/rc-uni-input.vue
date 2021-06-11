1e29b224d4589d6b45af51fce984361cf4fe7bb5
Ronchan0805 <488708238@qq.com>
2021-06-11 08:59:30 +0800

    vdom update async

---
+++ uni-input/my-alpha-project/components/rc-uni-input.vue
@@ -0,0 +1,62 @@
+<template>
+	<view class="ea-uni-box">
+
+		<input
+		 ref="input"
+     v-bind="$attrs"
+		 @input="handleInput"
+		 />
+
+	</view>
+</template>
+<script>
+	export default {
+
+		props: {
+			value: [String,Number]
+		},
+		watch: {
+
+		},
+		data() {
+			return {
+
+			}
+		},
+		computed: {
+      nativeInputValue() {
+        return this.value === null || this.value === undefined ? '' : String(this.value);
+      }
+		},
+		methods: {
+			// 输入框输入事件
+			handleInput (e) {
+        console.log(e)
+				this.$emit('input',e.detail.value);
+				this.$nextTick(() => {
+					this.$refs.input.valueSync = this.value;
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+.ea-uni-box {
+	width: 100%;
+	position: relative;
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+}
+.ea-uni-box input {
+	width: 100%;
+}
+.ea-uni-box .input-down {
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	z-index: 1;
+}
+</style>
---
+++ uni-input/my-alpha-project/src/pages/index/index.vue
@@ -0,0 +1,52 @@
+<template>
+	<view class="content">
+		<rc-uni-input v-model="data" @input="data=data.replace(/[^\d]/g,'')"></rc-uni-input>
+		<!-- <rc-uni-input v-model="data"></rc-uni-input> -->
+		data:{{data}}
+	</view>
+</template>
+
+<script>
+	import rcUniInput from "../../../components/rc-uni-input.vue"
+	export default {
+		components: {
+			rcUniInput
+		},
+		data() {
+			return {
+				data: ''
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin: 200rpx auto 50rpx auto;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
